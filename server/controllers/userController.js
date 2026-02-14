ce6a40f680f120a7625f2c5f9fa718f6f71e1d96
Prashant Kumar <prashant123@gmail.com>
2026-02-15 02:05:15 +0530

    initial commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -49,7 +49,7 @@ const updateProfile = async (req, res) => {
     if (email && email !== user.email) {
       const emailExists = await User.findOne({ email });
       if (emailExists) {
-        return res.status(400).json({
+        return res.status(400).js on({
           success: false,
           message: 'Email already in use'
         });

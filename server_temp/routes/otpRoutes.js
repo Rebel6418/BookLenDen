c464d8c308449fb6718cc799075584fae0532e0f
Prashant Kumar <prashant123@gmail.com>
2026-02-14 16:40:19 +0530

    Remove server submodule completely

--- server_temp/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendOTP, verifyOTP, resendOTP } = require('../controllers/otpController');
-
-router.post('/send-otp', sendOTP);
-router.post('/verify-otp', verifyOTP);
-router.post('/resend-otp', resendOTP);
-
-module.exports = router;
